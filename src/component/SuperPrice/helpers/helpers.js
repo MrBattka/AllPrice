0ce4f1d6bdc9358c6ee14b93d1d6d181907610b8
MrBaTTka <ilyha_96@inbox.ru>
2025-03-18 19:20:30 +0300

    fix

--- src/component/SuperPrice/helpers/helpers.js
+++ src/component/SuperPrice/helpers/helpers.js
@@ -35,7 +35,9 @@ export const fixNameSuperPrice = (name) => {
   const fixPixel5G =
     (fixXperia5G.indexOf("Pixel") != -1 ||
     fixXperia5G.indexOf("M55") != -1 ||
-    fixXperia5G.indexOf("S21") != -1)
+    fixXperia5G.indexOf("S21") != -1 ||
+    fixXperia5G.indexOf("A36") != -1 ||
+    fixXperia5G.indexOf("A56") != -1)
       ? fixXperia5G.replace("5G ", "")
       : fixXperia5G;
   const fixSilver = fixPixel5G.replace("Platinum Silver 12/512", "12/512 Platinum Silver");
